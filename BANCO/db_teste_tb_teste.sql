f422e54363b17fe966d21bc12abedbdee7f32ab7
Cesar Grossl <cgrossl00@gmail.com>
2022-06-16 07:35:40 -0300

    Finalização PDO

---
+++ BANCO/db_teste_tb_teste.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: db_teste
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_teste`
+--
+
+DROP TABLE IF EXISTS `tb_teste`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_teste` (
+  `tes_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tes_nome` varchar(200) DEFAULT NULL,
+  `tes_del` char(1) NOT NULL,
+  PRIMARY KEY (`tes_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_teste`
+--
+
+LOCK TABLES `tb_teste` WRITE;
+/*!40000 ALTER TABLE `tb_teste` DISABLE KEYS */;
+INSERT INTO `tb_teste` VALUES (1,'TESTE PDO',''),(2,'TESTE02',''),(3,'TESTE03',''),(4,'TESTE04','');
+/*!40000 ALTER TABLE `tb_teste` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-16  7:30:08
--- exemplos.php
+++ exemplos.php
@@ -1,17 +1,33 @@
 <?php
+//exmeplo simples efetuado com PDO, podemos melhorar esses recursos.
+$db_valida = "srv_portal";
 include_once("./PDO/db_banco.php");
 include_once("./PDO/pdo_conexao.php");
 $db = new DB();
 //exemplo 01 SELECT;
+echo '<br>Exemplo 01 -> select <br>';
 $db->AbreConexao('portal');
-    $qry_teste = $db->select("SELECT * FROM tb_teste ", "portal");
-    if (count($qry_teste)) {
-        foreach ($qry_teste as $conta_teste=>$res_teste) {
-            echo  $res_teste["tes_descricao"]); echo '<br>';
+    $qry=null;$row=null;$conta=null;
+    $qry = $db->select("SELECT tes_nome FROM tb_teste ", "portal");
+    if (count($qry)) {
+        foreach ($qry as $conta=>$row) {
+            echo  $row["tes_nome"]; echo '<br>';
         }
     }
 $db->FechaConexao('portal');
-
-//exemplo 02 UPDATE INSERT;
+//exemplo 02 UPDATE;
+echo '<br>Exemplo 02 -> insert e update <br>';
+$db->AbreConexao('portal');
+    $qry=null;
+    $qry = $db->query("UPDATE tb_teste SET tes_nome = 'TESTE PDO' WHERE tes_id = 1 ", "portal");
+    echo $qry;
+$db->FechaConexao('portal');
+//exemplo 03 INSERT;
+echo '<br>Exemplo 02 -> insert e update <br>';
+$db->AbreConexao('portal');
+    $qry=null;
+    $qry = $db->query("INSERT INTO tb_teste (tes_nome, tes_del)VALUES('TESTE05','') ", "portal");
+    echo $qry;
+$db->FechaConexao('portal');
 
 ?>
